fbc7d8e8f3669912e851437df1019554a349aef7
jsonLiang <yuanyu90221@gmail.com>
2020-06-28 17:08:32 +0800

    add db.disconnect

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yuanyu90221/golang_with_mongo_demo
 go 1.14
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/klauspost/compress v1.10.10 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	go.mongodb.org/mongo-driver v1.3.4 // indirect
--- main.go
+++ main.go
@@ -4,9 +4,8 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
-	_ "github.com/joho/godotenv/autoload"
+	config "github.com/yuanyu90221/golang_with_mongo_demo/config"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -18,14 +17,7 @@ type Cat struct {
 }
 
 func main() {
-	// load
-	DB_HOST := os.Getenv("MONGO_HOST")
-	DB_PORT := os.Getenv("PORT")
-	DB_NAME := os.Getenv("DB_NAME")
-	DB_USERBAME := os.Getenv("MONGO_INITDB_ROOT_USERNAME")
-	DB_PASSWD := os.Getenv("MONGO_INITDB_ROOT_PASSWORD")
-	connectString := fmt.Sprintf("mongodb://%s:%s@%s:%s/%s", DB_USERBAME, DB_PASSWD, DB_HOST, DB_PORT, DB_NAME)
-	log.Println("connectString", connectString)
+	connectString := config.GetConnectString()
 	clientOptions := options.Client().ApplyURI(connectString)
 	client, err := mongo.Connect(context.TODO(), clientOptions)
 	if err != nil {
@@ -45,5 +37,9 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("found single document: ", result)
+	err = client.Disconnect(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("found single document Name: ", result.Name, ",Age:", result.Age)
 }
