d12959e63f98ab3a39c85b3fe213bc71fb341c45
jsonLiang <yuanyu90221@gmail.com>
2020-06-29 01:18:41 +0800

    add connect to mongo main db

--- main.go
+++ main.go
@@ -4,63 +4,36 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	config "github.com/yuanyu90221/golang_with_mongo_demo/config"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
-
-type Cat struct {
-	Name string
-	Age  int
-}
 
 func main() {
 	connectString := config.GetConnectString()
 	clientOptions := options.Client().ApplyURI(connectString)
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+	client, err := mongo.NewClient(clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	err = client.Ping(context.TODO(), nil)
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println("Connected To MognoDB with ", connectString)
-	cats := client.Database("test").Collection("cats")
-	filter := bson.M{}
-	// var result Cat
-	var resultList []Cat
-	// err = cats.FindOne(context.TODO(), filter).Decode(&result)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	cur, errAll := cats.Find(context.Background(), filter)
-	if errAll != nil {
-		log.Fatal(errAll)
-	}
-	for cur.Next(context.Background()) {
-		var cat Cat
-		errTemp := cur.Decode(&cat)
-		if errTemp != nil {
-			log.Fatal(errTemp)
-		}
-		resultList = append(resultList, cat)
-	}
-	if len(resultList) > 0 {
-		deleteResult, err1 := cats.DeleteMany(context.TODO(), filter)
-		if err1 != nil {
-			log.Fatal(err1)
-		}
-		log.Println(*&deleteResult.DeletedCount)
-	}
-	err = client.Disconnect(context.Background())
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
 	if err != nil {
 		log.Fatal(err)
 	}
-	// fmt.Println("found single document Name: ", result.Name, ",Age:", result.Age)
-	fmt.Println(resultList)
+	database, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(database)
 }
